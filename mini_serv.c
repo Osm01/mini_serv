84042abd9e95fe84aa9aca5c8c524beb09700365
OSMOS <44633928+Osm01@users.noreply.github.com>
2025-04-02 16:33:38 +0000

    Update mini_serv.c

--- mini_serv.c
+++ mini_serv.c
@@ -21,34 +21,6 @@ t_users	users[1024];
 int		MaxFDs = 0;
 int		ids = 0;
 
-int extract_message(char **buf, char **msg)
-{
-	char	*newbuf;
-	int	i;
-
-	*msg = 0;
-	if (*buf == 0)
-		return (0);
-	i = 0;
-	while ((*buf)[i])
-	{
-		if ((*buf)[i] == '\n')
-		{
-			newbuf = calloc(1, sizeof(*newbuf) * (strlen(*buf + i + 1) + 1));
-			if (newbuf == 0)
-				return (-1);
-			strcpy(newbuf, *buf + i + 1);
-			*msg = *buf;
-			(*msg)[i + 1] = 0;
-			*buf = newbuf;
-			return (1);
-		}
-		i++;
-	}
-	return (0);
-}
-
-
 void	print_error(char *msg)
 {
 	write(2, msg, strlen(msg));
